296118fc42ff3e558e2152023b3ef3a2f42cc217
Eli Joesph Bradley <elijos03@gmail.com>
2017-07-15 10:50:35 -0700

    Basic Node.js key value store

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var url = require('url');
+
+app.set('port', (process.env.PORT || 5000));
+
+global.credentials = {};
+
+app.get('/credentials', function (req, res) {
+	Object.assign(global.credentials, req.query);
+	res.send(global.credentials);
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
